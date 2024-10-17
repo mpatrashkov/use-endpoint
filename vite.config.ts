3dd7d515a1aeef1e524b409c541738d1cbc8108b
miro-t212 <miroslav.patrashkov@trading212.com>
2024-10-17 20:57:52 +0300

    setup npm package

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		dts({ rollupTypes: true, tsconfigPath: "./tsconfig.app.json" }),
+	],
 	test: {
 		environment: "jsdom",
 	},
